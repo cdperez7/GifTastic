1a5e822a9039e969b643fd7b0847251a705d0d2d
C Perez <connorperez@gmail.com>
2019-04-19 17:18:13 -0500

    Java done, css and styling needed

--- assets/java.js
+++ assets/java.js
@@ -1,117 +1,102 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-	var topics = ["eggs benedict", "lobster", "Macaroni and cheese", "steak", "fried chicken", "calamari", "charcuterie", "risotto", "burger", "tacos", "gumbo"];
+        // Array of foods to be populated as gifs
+    	var foodArray = ["Eggs benedict", "Lobster", "Macaroni and cheese", "Steak", "Fried chicken", "Calamari", "Charcuterie", "Risotto", "Burger", "Tacos", "Crawfish"];
 
-	// Add buttons for original food array
-	function renderButtons() {
-		$("#food-buttons").empty();
-		for (i = 0; i < topics.length; i++) {
-			$("#food-buttons").append("<button class='btn btn-success' data-food='" + topics[i] + "'>" + topics[i] + "</button>");
-		}
-}
 
-renderButtons();
+        // First step was creating buttons to add to top of page
+        function createButtons(){
+            $("#food-buttons").empty();
 
-	// Adding a button for food entered
-	$("#add-food").on("click", function () {
-		event.preventDefault();
-		var food = $("#food-input").val().trim();
-		topics.push(food);
-		renderButtons();
-		return;
-});
+            // Looping through array and created button for each food
+            for (var i = 0; i < foodArray.length; i++){
+                var foodButton = $("<button>");
 
-$("button").on("click", function () {
-    var food = $(this).attr("data-food");
-    console.log(food);
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        food + "&api_key=QLyUumuGk3jmUU0HGF4Ba58A5vEgbkcj&limit=10"
+                //adding classes and attributes to each food to be referenced later on
+                foodButton.addClass("food");
+                foodButton.attr("food-name", foodArray[i]);
+                foodButton.text(foodArray[i]);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
+                //appending the buttons for each food
+                $("#food-buttons").append(foodButton);
+            }
+        }
 
-    .then(function(response) {
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
+        //Takes input from form in HTML to add a new button to page/array
+        $("#add-food").on("click", function(event){
+            event.preventDefault();
+            var food = $("#food-input").val().trim();
 
-            var gifDiv = $("<div>");
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
-            var foodImage = $("<img>");
-            foodImage.attr("src", results[i].images.original_still.url);
-            foodImage.attr("data-still", results[i].images.original_still.url);
-            foodImage.attr("data-animate", results[i].images.original.url);
-            foodImage.attr("data-state", "still");
-            foodImage.attr("class", "gif");
-            gifDiv.append(p);
-            gifDiv.append(foodImage);
-
-            $("#gifs-appear-here").prepend(gifDiv);
-          }
+            //adds food added in food input and pushes to the array and create button for what was added
+            foodArray.push(food);
+            $("#food-input").val(" ");
+            createButtons();
         });
-    });
 
-    function changeState(){
-        var state = $(this).attr("data-state");
-        console.log(state);
-        var animateImage = $(this).attr("data-animate");
-        var stillImage = $(this).attr("data-still");
-    
-        if (state == "still") {
-            $(this).attr("src", animateImage);
-            $(this).attr("data-state", "animate");
+        //main function, gets gifs, animates/stills, etc.
+        function addGifs(){
+            var food = $(this).attr("food-name");
+
+                //AJAX call + my giphy personal key - limit to 10 gifs per button
+                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                food + "&api_key=QLyUumuGk3jmUU0HGF4Ba58A5vEgbkcj&limit=10";
+
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+
+                //empty gifs so that if you click many buttons you won't have 70 gifs on your page    
+                }).done(function(response){
+                    $("#gifs-appear-here").empty();
+
+                    //targetting the information in the Gifs code
+                    var results = response.data;
+
+                    //loops through gif data and creates new div to display what the user clicks
+                    for (var i = 0; i < results.length; i++){
+                        var foodDiv = $("<div class='chosenFood'>");
+                    
+                    //targets the rating section within the gif and create a new p tag to add in below the gif
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    //specifies where to grab the still/animate data from the gif
+                    var still = results[i].images.fixed_height_still.url;
+                    var animate = results[i].images.fixed_height.url;
+
+                    //toughest part for me in this hw that I had to find working examples for - creates the gif div, and adds attributes, sets the attributes for both still and animate. Last part made a string to reference the pausing/animating clicks later on. 
+                    var gif = $("<img>").addClass("gif").attr("src", still).attr("data-still", still).attr("data-animate", animate).attr("data-state", "still");
+
+                    //now we append the new div with the gif and the rating below it
+                    foodDiv.append(gif);
+                    foodDiv.append(p);
+
+                    //attach to the correct spot in the html
+                    $("#gifs-appear-here").append(foodDiv);
+                }
+
+                //specifies what happens when the gif is clicked by matching the addedClass above
+                $(".gif").on("click", function(){
+
+                    //using "this" keyword so that whatever is clicked is what is referenced for still/animate
+                    var state = $(this).attr("data-state");
+
+                    //if statement that will make still gifs animate and vice versa
+                    if (state === "still"){
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                    }
+                });
+
+                });
         }
-    
-        else if (state == "animate") {
-            $(this).attr("src", stillImage);
-            $(this).attr("data-state", "still");
-        }
-    }
-    
-    
-    $(document).on("click", ".gif", changeState);
+        //specifies what happens when buttons are clicked - .food class was added when buttons were created
+        $(document).on("click", ".food", addGifs);
 
-
+        //running the function to create buttons when the page is started
+        createButtons();
 
 });
-
-
-
-function changeState(){
-    var state = $(this).attr("data-state");
-    var animateImage = $(this).attr("data-animate");
-    var stillImage = $(this).attr("data-still");
-
-    if (state == "still") {
-        $(this).attr("src", animateImage);
-        $(this).attr("data-state", "animate");
-    }
-
-    else if (state == "animate") {
-        $(this).attr("src", stillImage);
-        $(this).attr("data-state", "still");
-    }
-}
-
-
-$(document).on("click", ".gif", changeState);
-
-// });
-
-
-// $(document).on("click", ".foodImage", function() {
-//     var state = $(this).attr("data-state");
-//     console.log("test");
-//     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-//     // Then, set the image's data-state to animate
-//     // Else set src to the data-still value
-//     if (state === "still") {
-//       $(this).attr("src", $(this).attr("data-animate"));
-//       $(this).attr("data-state", "animate");
-//     } else {
-//       $(this).attr("src", $(this).attr("data-still"));
-//       $(this).attr("data-state", "still");
-//     }
-//   });
